b5e91bc7dbc4ea74bb3689597d77dff910b0cd81
Gustavo <gstvcaixeta@gmail.com>
2024-10-04 12:39:26 -0300

    Project init

---
+++ src/main/java/caixeta/gustavo/biblioteca/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package caixeta.gustavo.biblioteca.repository;
+
+import caixeta.gustavo.biblioteca.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitle(String title);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByQuantityAvailableGreaterThan(int quantity);
+}
